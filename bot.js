605f4b6a5d1c9c28f72b7eeb32e790e320f1ec59
kokoseda <kokoseda22@yahoo.com>
2018-11-14 22:58:47 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,11 @@
 const Discord = require("discord.js")
 const client = Discord.Client();
-const.log = ("hi")
+console.log('hi');
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag} !`);
+ 
+});
 
 
 client.on('message', message => {
