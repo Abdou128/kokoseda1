cee239e256d9e5856a99191758a2c117302ca79a
kokoseda <kokoseda22@yahoo.com>
2018-11-15 00:57:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,9 @@
 const Discord = require("discord.js")
-const client = new Discord.Client();
+const bot = new Discord.Client();
 console.log('hi');
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag} !`);
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag} !`);
  
 });
 
@@ -16,10 +16,6 @@ const randomWord = require("random-words")
 
 
 
-
-
-
-
 var spam;
 
 var phrases = ["thing1","another one","keep adding more","there's no limit"];
@@ -27,9 +23,9 @@ var phrases = ["thing1","another one","keep adding more","there's no limit"];
 function startspam()
 {
     console.log("Spam starting!")
-    var server = "495608433064673281"; // ايدي السررفر
-    var chan = "503222054946996224";//ايدي الروم
-    spam = client.setInterval(()=>
+    var server = bot.guilds.get("510568893975232531");
+    var chan = new discord.TextChannel(server,{"id":"510568894516166707"});
+    spam = bot.setInterval(()=>
     {
         chan.send(randomWord()).then(msg=>{ // Sticking with randomwords.
             console.log(msg.content);
@@ -40,15 +36,15 @@ function startspam()
 
 function stopspam()
 {
-    client.clearTimeout(spam);
+    bot.clearTimeout(spam);
     console.log("Spam Stopped.")
 }
 
-client.on("ready",()=>{
+bot.on("ready",()=>{
     console.log("Ready!");
 })
 
-client.on("message",msg=>{
+bot.on("message",msg=>{
     if(msg.author.id == "314135031029170197")
     {
         if(msg.content.toLowerCase() == "1start")
@@ -62,7 +58,12 @@ client.on("message",msg=>{
     }
 })
 
-client.on('message', message => {
+
+
+
+ 
+
+bot.on('message', message => {
 if(message.content.startsWith(prefix + 's')) {
 if(message.author.id !== "314135031029170197") return;
 var args = message.content.split(' ').slice(1).join(' ');
@@ -70,31 +71,31 @@ message.channel.send(args);
 }
 });
 
-client.on('message', message => {
+bot.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
       if (!devs.includes(message.author.id)) return;
       
 if (message.content.startsWith('setavatar')) {
-  client.user.setAvatar(argresult);
+  bot.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
 
 
 
-client.on('ready', async() => {
+bot.on('ready', async() => {
 var server = "495608433064673281"; // ايدي السررفر
 var channel = "503222054946996224";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#daily')
+    bot.guilds.get(server).channels.get(channel).send('#daily')
     },8.64e+7);
 })
 
-client.on('ready', async() => {
+bot.on('ready', async() => {
 var server = "495608433064673281"; // ايدي السررفر
 var channel = "503222054946996224";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('#rep <@314135031029170197>')
+    bot.guilds.get(server).channels.get(channel).send('#rep <@314135031029170197>')
     },8.64e+7);
 })
 
