317d068c4aa23ac79efdc6557365a7c5f9a2db1c
kokoseda <kokoseda22@yahoo.com>
2018-11-15 11:16:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -49,5 +49,42 @@ bot.on("message",msg=>{
 })
 
 
+bot.on('message', message => {
+if(message.content.startsWith(prefix + 's')) {
+if(message.author.id !== "314135031029170197") return;
+var args = message.content.split(' ').slice(1).join(' ');
+message.channel.send(args);
+}
+});
+
+bot.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+if (message.content.startsWith('setavatar')) {
+  bot.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
+
+
+bot.on('ready', async() => {
+var server = "495608433064673281"; // ايدي السررفر
+var channel = "503222054946996224";//ايدي الروم
+    setInterval(()=>{
+    bot.guilds.get(server).channels.get(channel).send('#daily')
+    },8.64e+7);
+})
+
+bot.on('ready', async() => {
+var server = "495608433064673281"; // ايدي السررفر
+var channel = "503222054946996224";//ايدي الروم
+    setInterval(()=>{
+    bot.guilds.get(server).channels.get(channel).send('#rep <@314135031029170197>')
+    },8.64e+7);
+})
+
+
 
 bot.login(process.env.BOT_TOKEN);
