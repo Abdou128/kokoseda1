08944d88242f4eb14814146af65bf4f93cb2d0a5
kokoseda <kokoseda22@yahoo.com>
2018-12-30 19:48:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,18 +16,6 @@ bot.login(process.env.BOT_TOKEN8);
 bot.login(process.env.BOT_TOKEN9);
 bot.login(process.env.BOT_TOKEN10);
 bot.login(process.env.BOT_TOKEN11);
-bot.login(process.env.BOT_TOKEN12);
-bot.login(process.env.BOT_TOKEN13);
-bot.login(process.env.BOT_TOKEN14);
-bot.login(process.env.BOT_TOKEN15);
-bot.login(process.env.BOT_TOKEN16);
-bot.login(process.env.BOT_TOKEN17);
-bot.login(process.env.BOT_TOKEN18);
-bot.login(process.env.BOT_TOKEN19);
-bot.login(process.env.BOT_TOKEN20);
-bot.login(process.env.BOT_TOKEN21);
-bot.login(process.env.BOT_TOKEN22);
-bot.login(process.env.BOT_TOKEN23);
 
 var spam;
 
